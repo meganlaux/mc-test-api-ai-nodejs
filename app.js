774f4ab6531f27630f672c9e5202614383a1258e
Megan Laux <megan.laux@mastercard.com>
2017-04-07 10:24:30 -0500

    m

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res) {
